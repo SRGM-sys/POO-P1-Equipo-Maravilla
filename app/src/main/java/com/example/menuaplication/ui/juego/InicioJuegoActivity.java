9e8c1854b8e276e73db5105c0a456eddb1a533cf
ThaMatthias <156712078+TheMattias1127@users.noreply.github.com>
2026-01-17 18:12:23 -0500

    Update InicioJuegoActivity.java
    Remodelación de la clase InicioJuegoActivity

--- app/src/main/java/com/example/menuaplication/ui/juego/InicioJuegoActivity.java
+++ app/src/main/java/com/example/menuaplication/ui/juego/InicioJuegoActivity.java
@@ -2,7 +2,6 @@ package com.example.menuaplication.ui.juego;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.menuaplication.R;
@@ -16,15 +15,10 @@ public class InicioJuegoActivity extends AppCompatActivity {
 
         Button btnIniciar = findViewById(R.id.btn_iniciar_juego);
 
-        // Usamos una clase anónima o lambda para el click.
-        // El Intent es el mensaje que le dice a Android "Quiero abrir esta otra pantalla".
-        btnIniciar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(InicioJuegoActivity.this, JuegoMemoriaActivity.class);
+        btnIniciar.setOnClickListener( v -> {
+            Intent intent = new Intent(InicioJuegoActivity.this, JuegoMemoriaActivity.class);
                 startActivity(intent);
-                // No ponemos finish() aquí si queremos que al volver atrás regrese a esta pantalla.
-            }
+                // No ponemos finish() aquí si queremos que al volver atrás regrese a esta pantalla
         });
     }
 }
