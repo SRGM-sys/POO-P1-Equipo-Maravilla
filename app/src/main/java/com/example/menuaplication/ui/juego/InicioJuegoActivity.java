2b2f317a02fec811ab409defbcc36c2e0450f345
ThaMatthias <156712078+TheMattias1127@users.noreply.github.com>
2026-01-17 19:05:45 -0500

    Implementamos una opción para volver a las actividades anteriores

--- app/src/main/java/com/example/menuaplication/ui/juego/InicioJuegoActivity.java
+++ app/src/main/java/com/example/menuaplication/ui/juego/InicioJuegoActivity.java
@@ -3,6 +3,7 @@ package com.example.menuaplication.ui.juego;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ImageButton;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.menuaplication.R;
 
@@ -14,11 +15,16 @@ public class InicioJuegoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_inicio_juego);
 
         Button btnIniciar = findViewById(R.id.btn_iniciar_juego);
+        ImageButton btnVolver = findViewById(R.id.btn_volver_menu);
 
         btnIniciar.setOnClickListener( v -> {
             Intent intent = new Intent(InicioJuegoActivity.this, JuegoMemoriaActivity.class);
                 startActivity(intent);
                 // No ponemos finish() aquí si queremos que al volver atrás regrese a esta pantalla
+        });
+
+        btnVolver.setOnClickListener(v -> {
+            finish(); // Simplemente cierra esta actividad para volver a la anterior
         });
     }
 }
